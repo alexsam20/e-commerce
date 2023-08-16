f73565cc9ccfe0da5c75a08a81cefbd9e276f279
Alexandr SaM <alexsam2070@gmail.com>
2023-08-16 14:14:40 -0400

    Create new User

---
+++ utility/create-user.php
@@ -0,0 +1,22 @@
+<?php
+
+use Doctrine\ORM\EntityManager;
+
+require_once 'vendor/autoload.php';
+require_once 'bootstrap.php';
+
+[$filename, $username, $password] = $argv;
+
+$user = new \App\Entity\User();
+
+$hashedPassword = password_hash($password, PASSWORD_DEFAULT);
+
+$user->setUsername($username);
+$user->setPassword($hashedPassword);
+
+/** @var EntityManager $em */
+$em = $entityManager;
+$em->persist($user);
+$em->flush();
+
+echo 'Created user with id ' . $user->getId() . PHP_EOL;
