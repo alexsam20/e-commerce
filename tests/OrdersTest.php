a3f6c818f337ad892103e63215bd4f80df973e3c
Alexandr SaM <alexsam2070@gmail.com>
2023-08-17 19:54:39 -0400

    Order Test

--- tests/OrdersTest.php
+++ tests/OrdersTest.php
@@ -2,31 +2,60 @@
 
 namespace App\Tests;
 
+use App\Entity\Order;
 use App\Entity\Product;
 
 class OrdersTest extends DatabaseDependantTastCase
 {
-    /** @test  */
-    public function an_order_can_be_created()
+    private string $deliveryName = 'Ringo Starr';
+    private string $deliveryAddress = '44 Penny Line, Liverpool';
+
+    protected function setUp(): void
     {
-        // SETUP
-        $deliveryName = 'Ringo Starr';
-        $deliveryAddress = '44 Penny Line, Liverpool';
+        parent::setUp();
 
         $order = new \App\Entity\Order();
-        $order->setDeliveryName($deliveryName);
-        $order->setDeliveryaddress($deliveryAddress);
+        $order->setDeliveryName($this->deliveryName);
+        $order->setDeliveryaddress($this->deliveryAddress);
 
         // DO SOMETHING
         $this->entityManager->persist($order);
         $this->entityManager->flush();
+    }
 
-        //MAKE ASSERTIONS
-        //$this->assertSame(1, $product->getId());
-
+    /** @test  */
+    public function an_order_can_be_created()
+    {
+        // MAKE ASSERTIONS
         $this->assertDatabaseHas(\App\Entity\Order::class, [
-            'deliveryName' => $deliveryName,
-            'deliveryAddress' => $deliveryAddress,
+            'deliveryName' => $this->deliveryName,
+            'deliveryAddress' => $this->deliveryAddress,
+        ]);
+    }
+
+    /** @test  */
+    public function an_order_can_be_updated()
+    {
+        // SETUP
+        /** @var Order $order */
+        $order =  $this->entityManager->getRepository(Order::class)->findOneBy([
+            'deliveryName' => $this->deliveryName,
+        ]);
+        $newAddress = '9 Newcastle Avenue, Liverpool';
+
+        // DO SOMETHING
+        $order->setDeliveryAddress($newAddress);
+        $this->entityManager->persist($order);
+        $this->entityManager->flush();
+
+        // MAKE ASSERTIONS
+        $this->assertDatabaseHas(\App\Entity\Order::class, [
+            'deliveryName' => $this->deliveryName,
+            'deliveryAddress' => $newAddress,
+        ]);
+        $this->assertDatabaseNotHas(\App\Entity\Order::class, [
+            'deliveryName' => $this->deliveryName,
+            'deliveryAddress' => $this->deliveryAddress,
         ]);
     }
 }
