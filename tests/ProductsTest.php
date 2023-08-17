45fa3af0800f084337a918900e3f8bf591e67d78
Alexandr SaM <alexsam2070@gmail.com>
2023-08-16 20:33:34 -0400

    Create product test

--- tests/ProductsTest.php
+++ tests/ProductsTest.php
@@ -2,11 +2,47 @@
 
 namespace App\Tests;
 
+use App\Entity\Product;
+
 class ProductsTest extends DatabaseDependantTastCasde
 {
     /** @test */
-    public function testItWorks()
+    public function a_product_can_be_created()
     {
-        $this->assertTrue(true);
+        // SETUP
+        $name = 'Roland TD-07KV V-Drum Electronic Drum Kit BUNDLE';
+        $description = 'If you don’t want to preface the docker command with sudo, create a Unix group called docker and add users to it. When the Docker daemon starts, it creates a Unix socket accessible by members of the docker group. On some Linux distributions, the system automatically creates this group when installing Docker Engine using a package manager. In that case, there is no need for you to manually create the group.';
+
+        $product = new Product();
+        $product->setName($name);
+        $product->setDescription($description);
+        $product->setPrice(94400);
+
+        // DO SOMETHING
+        $this->entityManager->persist($product);
+        $this->entityManager->flush();
+
+        //MAKE ASSERTIONS
+        $this->assertSame(1, $product->getId());
+    }
+
+    /** @test */
+    public function duplicate()
+    {
+        // SETUP
+        $name = 'Roland TD-07KV V-Drum Electronic Drum Kit BUNDLE';
+        $description = 'If you don’t want to preface the docker command with sudo, create a Unix group called docker and add users to it. When the Docker daemon starts, it creates a Unix socket accessible by members of the docker group. On some Linux distributions, the system automatically creates this group when installing Docker Engine using a package manager. In that case, there is no need for you to manually create the group.';
+
+        $product = new Product();
+        $product->setName($name);
+        $product->setDescription($description);
+        $product->setPrice(94400);
+
+        // DO SOMETHING
+        $this->entityManager->persist($product);
+        $this->entityManager->flush();
+
+        //MAKE ASSERTIONS
+        $this->assertSame(1, $product->getId());
     }
 }
