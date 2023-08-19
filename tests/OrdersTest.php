4949751136bfafc79bbbd5c3894d0581dabd6421
Alexandr SaM <alexsam2070@gmail.com>
2023-08-19 19:59:43 -0400

    Adding Multiple Items

--- tests/OrdersTest.php
+++ tests/OrdersTest.php
@@ -125,4 +125,50 @@ class OrdersTest extends DatabaseDependantTastCase
         // Check that we can retrieve items for an e,g, #order->getItems()
         $this->assertCount(1, $order->getItems());
     }
+
+    /** @test  */
+    public function multiple_items_can_be_added_to_an_order()
+    {
+        // SETUP
+        $multiple = 3;
+        // Need a product
+        $name = 'Roland TD-07KV V-Drum Electronic Drum Kit BUNDLE';
+        $description = 'If you don’t want to preface the docker command with sudo, create a Unix group called docker and add users to it. When the Docker daemon starts, it creates a Unix socket accessible by members of the docker group. On some Linux distributions, the system automatically creates this group when installing Docker Engine using a package manager. In that case, there is no need for you to manually create the group.';
+
+        $product = new Product();
+        $product->setName($name);
+        $product->setDescription($description);
+        $product->setPrice(94400);
+        $this->entityManager->persist($product);
+        $this->entityManager->flush();
+
+        // Need an order
+        /** @var Order $order */
+        $order =  $this->entityManager->getRepository(Order::class)->findOneBy([
+            'deliveryName' => $this->deliveryName,
+        ]);
+
+        // DO SOMETHING
+        // Create an item using refs to the order and product
+        for ($i = 1; $i <= $multiple; $i++) {
+
+            $item = new Item();
+            $item->setOrder($order);
+            $item->setProduct($product);
+            $item->setPrice($product->getPrice());
+
+            $this->entityManager->persist($item);
+        }
+
+        $this->entityManager->flush();
+
+        // MAKE ASSERTIONS
+        // Check that the item has been created
+        $this->assertDatabaseHas(Item::class, [
+            'price' => $product->getPrice(),
+        ]);
+
+        // Check that we can retrieve items for an e,g, #order->getItems()
+        $this->assertCount($multiple, $order->getItems());
+    }
 }
